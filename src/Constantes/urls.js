2b9401539dea655ddb8acd37b425ab009fcbebf7
Layla Borges <layla.hissa82@gmail.com>
2024-11-08 17:07:28 -0300

    teste de url

--- src/Constantes/urls.js
+++ src/Constantes/urls.js
@@ -1,2 +1,5 @@
-export const BASE_URL = "https://pokeapi.co/api/v2";
+//export const BASE_URL = "https://pokeapi.co/api/v2";
+
+export const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3003";
+
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
-import axios from "axios";
+/*import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://pokeapi.co/api/v2/",
+});
+
+export default api;
+*/
+
+import axios from "axios";
+import { BASE_URL } from "../Constantes/urls";
+
+const api = axios.create({
+  baseURL: BASE_URL,
 });
 
 export default api;
